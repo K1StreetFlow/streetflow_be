ed0a9aa0cc354838b80d1b2274cc74310a2b5f24
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2023-11-08 20:41:06 +0700

    mengedit file config/database

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "postgres",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || "streetflow",
     host: process.env.DB_HOST || "localhost",
     port: 5432,
