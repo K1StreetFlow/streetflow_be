c27aa3f9cf89fdfcb473f88793bcd990d080fae5
ReCodink <muhammadraihan1607@gmail.com>
2023-11-08 19:46:49 +0700

    Add Migration Review_products

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-<<<<<<< HEAD
-	development: {
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME || "streetflow",
-		host: process.env.DB_HOST || "localhost",
-		port: 5432,
-		dialect: "postgres",
-	},
-=======
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
@@ -19,5 +9,4 @@ module.exports = {
     port: 5432,
     dialect: "postgres",
   },
->>>>>>> raihan
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,12 @@
 const route = require("express").Router();
 const testingRoutes = require("./testing.routes");
-<<<<<<< HEAD
 const orderListRoutes = require("./order_list.routes");
 const productsListRoutes = require("./products.routes");
-
-
-route.use("/", testingRoutes);
-route.use("/order", orderListRoutes);
-route.use("/products", productsListRoutes);
-
-
-=======
 const reviewProductsRoutes = require('./Review_products_routes');
 
-
+route.use("/", testingRoutes);
 route.use("/testing", testingRoutes);
+route.use("/order", orderListRoutes);
+route.use("/products", productsListRoutes);
 route.use("/review_products", reviewProductsRoutes);
->>>>>>> raihan
 module.exports = route;
