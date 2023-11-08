0f72d340f0dc96ad416d79a2facb0c8ab82b2e82
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2023-11-08 21:38:10 +0700

    menambahkan route payment

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,18 @@ const route = require("express").Router();
 const testingRoutes = require("./testing.routes");
 const orderListRoutes = require("./order_list.routes");
 const productsListRoutes = require("./products.routes");
-const reviewProductsRoutes = require('./review_products.routes');
+const reviewProductsRoutes = require("./review_products.routes");
+const paymentRoutes = require("./payment.routes");
+const cartRoutes = require("./cart.routes");
+const cartDetailRoutes = require("./cart-detail.routes");
 
 route.use("/", testingRoutes);
 route.use("/testing", testingRoutes);
 route.use("/order", orderListRoutes);
 route.use("/products", productsListRoutes);
 route.use("/review_products", reviewProductsRoutes);
+route.use("/payments", paymentRoutes);
+route.use("/carts", cartRoutes);
+route.use("/cart-details", cartDetailRoutes);
 
 module.exports = route;
