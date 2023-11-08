c469f80292b954af2965da6f568f13b08da029ac
ReCodink <muhammadraihan1607@gmail.com>
2023-11-08 18:38:22 +0700

    Add Migration Review_products

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,7 @@ const testingRoutes = require("./testing.routes");
 const reviewProductsRoutes = require('./Review_products_routes');
 
 route.use("/testing", testingRoutes);
+
+
 route.use("/review_products", reviewProductsRoutes);
 module.exports = route;
