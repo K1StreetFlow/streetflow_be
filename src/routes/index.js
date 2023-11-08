b09111f58675bb119ba4b6b5f60d4e713d41d352
arifinkhoirul <irul30582@gmail.com>
2023-11-08 16:21:07 +0700

    tambah migrasi table products

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const route = require("express").Router();
 const testingRoutes = require("./testing.routes");
 const orderListRoutes = require("./order_list.routes");
+const productsListRoutes = require("./products.routes");
+
 
 route.use("/", testingRoutes);
 route.use("/order", orderListRoutes);
+route.use("/products", productsListRoutes);
+
 
 module.exports = route;
