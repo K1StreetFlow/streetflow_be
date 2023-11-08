cf5a8c28347c2f13aa7be7bec7e30ba3e61e5df9
ReCodink <muhammadraihan1607@gmail.com>
2023-11-08 19:11:50 +0700

    Add Migration Review_products

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 
 module.exports = {
   development: {
-    username: /* process.env.DB_USERNAME || */ "recodink",
-    password: /* process.env.DB_PASSWORD || */ "recodink16",
-    database: /* process.env.DB_NAME || */ "streetflow",
-    host: /*process.env.DB_HOST ||*/ "localhost",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || "streetflow",
+    host: process.env.DB_HOST || "localhost",
     port: 5432,
     dialect: "postgres",
   },
