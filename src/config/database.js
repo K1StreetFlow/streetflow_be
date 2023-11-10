4a2f8f8b026a195038fd162bcdf275fa5560f6de
Alvianori <alvianorck@gmail.com>
2023-11-10 19:56:51 +0700

    mengembalikan config/database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
+const fs = require("fs");
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
     database: process.env.DB_NAME || "streetflow",
     host: process.env.DB_HOST || "localhost",
     port: 5432,
