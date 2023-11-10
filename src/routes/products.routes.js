941c079df5beb0df119c90aac6de23742a06b2e1
Saeful mu'minin <saeful2026027@gmail.com>
2023-11-10 17:06:53 +0700

    tambah route untuk fitur crud

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,26 @@
 const express = require("express");
-const { getAllProducts, getProductById } = require("../controllers/products.controller");
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/products.controller");
 const router = express.Router();
 
+// Rute untuk mendapatkan semua produk
 router.get("/", getAllProducts);
+
+// Rute untuk mendapatkan produk berdasarkan ID
 router.get("/:id", getProductById);
+
+// Rute untuk membuat produk baru
+router.post("/", createProduct);
+
+// Rute untuk mengupdate produk berdasarkan ID
+router.put("/:id", updateProduct);
+
+// Rute untuk menghapus produk berdasarkan ID
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
