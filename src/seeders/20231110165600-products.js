c0ffa333881b92f6eb6955ddc8c792c8dd503cfb
Saeful mu'minin <saeful2026027@gmail.com>
2023-11-11 01:09:45 +0700

    membuat sedd product

--- src/seeders/20231110165600-products.js
+++ src/seeders/20231110165600-products.js
@@ -3,12 +3,24 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const categoryProducts = await queryInterface.sequelize.query(
+    const categoryProductsQuery = await queryInterface.sequelize.query(
       'SELECT id FROM "CategoryProducts"'
     );
-    const photoProducts = await queryInterface.sequelize.query(
+    const photoProductsQuery = await queryInterface.sequelize.query(
       'SELECT id FROM "PhotoProducts"'
     );
+
+    // Mendapatkan hasil query sebagai array
+    const categoryProducts = categoryProductsQuery[0];
+    const photoProducts = photoProductsQuery[0];
+
+    // Pastikan hasil query tidak kosong
+    if (categoryProducts.length === 0 || photoProducts.length === 0) {
+      console.error(
+        "Error: Empty result from categoryProducts or photoProducts query."
+      );
+      return;
+    }
 
     await queryInterface.bulkInsert(
       "Products",
@@ -19,12 +31,38 @@ module.exports = {
           price_product: 29.99,
           stock_product: 50,
           size_product: "M",
-          colour_product: "black",
-          id_category_products: categoryProducts[0][0].id,
-          id_photo_product: photoProducts[0][0].id,
+          color_product: "black",
+          id_category_product: categoryProducts[0].id,
+          id_photo_product: photoProducts[0].id,
           slug_product: "product-1",
-          createdAt: new Date(),
-          updatedAt: new Date(),
+          created_At: new Date(),
+          updated_At: new Date(),
+        },
+        {
+          name_product: "Product 2",
+          description_product: "Description for Product 2",
+          price_product: 39.99,
+          stock_product: 30,
+          size_product: "L",
+          color_product: "black",
+          id_category_product: categoryProducts[1].id,
+          id_photo_product: photoProducts[1].id,
+          slug_product: "product-2",
+          created_At: new Date(),
+          updated_At: new Date(),
+        },
+        {
+          name_product: "Product 3",
+          description_product: "Description for Product 3",
+          price_product: 49.99,
+          stock_product: 20,
+          size_product: "S",
+          color_product: "blue",
+          id_category_product: categoryProducts[2].id,
+          id_photo_product: photoProducts[2].id,
+          slug_product: "product-3",
+          created_At: new Date(),
+          updated_At: new Date(),
         },
       ],
       {}
