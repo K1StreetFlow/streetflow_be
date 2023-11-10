17f4b6a2a3125e725ca81f408420e1fd01ea3b73
Saeful mu'minin <saeful2026027@gmail.com>
2023-11-10 17:04:48 +0700

    beda pengaturan kayakya

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
     database: process.env.DB_NAME || "streetflow",
     host: process.env.DB_HOST || "localhost",
     port: 5432,
