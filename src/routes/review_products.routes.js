f33fe159c290ec7943b599d23152ba4867ffd6eb
ReCodink <muhammadraihan1607@gmail.com>
2023-11-12 20:55:36 +0700

    Update Router Review Products

--- src/routes/review_products.routes.js
+++ src/routes/review_products.routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const { getReviewProducts, getReviewProductById } = require('../controllers/review_product_controller');
-
+const { 
+    getReview, 
+    getReviewById, 
+    getReviewByRating, 
+    createReview, 
+    updateReview, 
+    deleteReview 
+} = require('../controllers/review_product_controller');
 const router = express.Router();
+const upload = require('../config/multer.config')
 
-router.get("/", getReviewProducts);
-router.get("/:id", getReviewProductById);
+router.get("/", getReview);
+router.get("/:id", getReviewById);
+router.get("/rating", getReviewByRating);
+router.post("/", createReview);
+router.put("/:id", updateReview);
+router.delete("/:id", deleteReview);
 
 module.exports = router;
