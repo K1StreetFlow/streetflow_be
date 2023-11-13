829912f1b3c941bc0da58e355ba5ff0f871b13e1
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2023-11-13 18:52:50 +0700

    menambahkan middleware user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const { getAllUsers, getUserById, editUser, deleteUser } = require("../controllers/userController");
-const { verifyTokenCookieAdmin, verifyTokenCookieCustomer } = require("../middleware/verifyToken");
+const {
+  getAllUsers,
+  getUserById,
+  editUser,
+  deleteUser,
+} = require("../controllers/userController");
+const {
+  verifyTokenCookieAdmin,
+  verifyTokenCookieCustomer,
+} = require("../middleware/verifyToken");
 const { isAdminOrSelf } = require("../middleware/adminMiddleware");
+const { isUser } = require("../middleware/userMiddleware");
 const upload = require("../middleware/multerConfig");
 const router = express.Router();
 
 router.get("/", verifyTokenCookieAdmin, getAllUsers);
-router.get("/:id", isAdminOrSelf, getUserById);
-router.put("/:id", isAdminOrSelf, upload.single("profileImage"), editUser);
-router.delete("/:id", isAdminOrSelf, deleteUser);
+router.get("/:id", isAdminOrSelf, isUser, getUserById);
+router.put(
+  "/:id",
+  isAdminOrSelf,
+  isUser,
+  upload.single("profileImage"),
+  editUser
+);
+router.delete("/:id", isAdminOrSelf, isUser, deleteUser);
 
 module.exports = router;
