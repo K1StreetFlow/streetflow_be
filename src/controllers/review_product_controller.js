e3f95615a55b006349e78aa64ac955502f0eeb60
ReCodink <muhammadraihan1607@gmail.com>
2023-11-14 12:31:08 +0700

    Update Controllers

--- src/controllers/review_product_controller.js
+++ src/controllers/review_product_controller.js
@@ -1,14 +1,20 @@
-const { Review_products, Order_list } = require('../models');
+const { Review_products, Users_customer, Product, Order_list } = require('../models');
 const upload = require('../config/multer.config')
 
 const getReview = async (req, res) => {
     try {
-        const reviews = await Review_products.findAll();
+        const reviews = await Review_products.findAll({
+            include: [
+                { model: Users_customer, as: 'users_customer', attributes: ['fullname']},
+                { model: Product, as: 'products', attributes: ['name_product']},
+                { model: Order_list, as: 'order_list', attributes: ['code_order']}
+            ]
+        });
 
-        if (reviews.length > 0) {
+        if (reviews) {
             res.status(200).json({
                 message: 'Get All Review Successfully',
-                data: reviewProducts,
+                data: reviews,
             });
         } else {
             res.status(404).json({
@@ -26,7 +32,13 @@ const getReview = async (req, res) => {
 const getReviewById = async (req, res) => {
     try {
         const { id } = req.params;
-        const review = await Review_products.findByPk(id);
+        const review = await Review_products.findByPk(id, {
+            include: [
+                { model: Users_customer, as: 'users_customer', attributes: ['fullname']},
+                { model: Product, as: 'products', attributes: ['name_product']},
+                { model: Order_list, as: 'order_list', attributes: ['code_order']}
+            ]
+        });
 
         if (review) {
             res.status(200).json({
@@ -48,7 +60,12 @@ const getReviewByRating = async (req, res) => {
         const reviews = await Review_products.findAll({
             where: {
                 number_review: rating,
-            }
+            },
+            include: [
+                { model: Users_customer, as: 'users_customer', attributes: ['fullname']},
+                { model: Product, as: 'products', attributes: ['name_product']},
+                { model: Order_list, as: 'order_list', attributes: ['code_order']}
+            ]
         });
 
         if (reviews.length > 0) {
@@ -67,19 +84,24 @@ const getReviewByRating = async (req, res) => {
 
 const createReview = async (req, res) => {
     try {
-        const { id_order_list, id_products, id_users_customer, message_review, number_review } = req.body;
-
-        const order = await Order_list.findByPk(id_order_list);
+        const { id_products, id_users_customer, message_review, number_review, id_order_list } = req.body;
 
         
-        // Cek apakah order sudah selesai
-        if (!order || order.status_order !== 'Finished') {
+        // Query the Order_list model
+        const order = await Order_list.findOne(id_order_list);
+
+        console.info("Request Body:", req.body);
+        console.info("ID Order List:", id_order_list);
+        console.info("Order Status:", order ? order.status_order : "Order not found");
+
+        // Check if the order is completed
+        if (!order || order.status_order !== 'Completed') {
             return res.status(400).json({
-                message: "Order not finished or not found"
+                message: `Order not completed or not found`
             });
         }
 
-        // Cek apakah review sudah pernah dibuat untuk produk yang sama oleh pelanggan yang sama
+        // Check if a review already exists for the same product by the same customer
         const existingReview = await Review_products.findOne({
             where: {
                 id_products,
@@ -88,28 +110,29 @@ const createReview = async (req, res) => {
         });
 
         if (existingReview) {
-            return res.status(400).json({ message: "Review for this product already exists"});
+            return res.status(400).json({ message: "Review for this product already exists" });
         }
 
-        // Menggunakan middleware multer untuk mengunggah file
+        // Use middleware multer to upload files
         upload(req, res, async (err) => {
             if (err) {
                 console.error(err);
-                return res.status(500).json({ message: "Error uploading photo"});
+                return res.status(500).json({ message: "Error uploading photo" });
             }
-            
+
+            // Create a new review
             const reviewProducts = await Review_products.create({
-                id_products,
                 id_users_customer,
+                id_products,
                 id_order_list,
                 message_review,
                 number_review,
-                photo_review
+                photo_review,
             });
 
             if (reviewProducts) {
                 res.status(201).json({
-                    message: "Create Review Products Succesfully",
+                    message: "Create Review Products Successfully",
                     data: reviewProducts
                 });
             } else {
@@ -120,46 +143,64 @@ const createReview = async (req, res) => {
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal Server Error "})
+        res.status(500).json({ message: "Internal Server Error " });
     }
 };
+
 
 const updateReview = async (req, res) => {
     try {
         const { id } = req.params;
-        const { message_review, number_review } = req.body
+        const { message_review, number_review } = req.body;
 
         // Cek apakah review sudah ada
         const review = await Review_products.findByPk(id);
 
         if (!review) {
-            return res.status(404).json({ message: "Review not found"});
+            return res.status(404).json({ message: "Review not found" });
+        }
+
+        // Update review attributes if provided in the request body
+        if (message_review !== undefined) {
+            review.message_review = message_review;
+        }
+
+        if (number_review !== undefined) {
+            review.number_review = number_review;
         }
 
         // Menggunakan middleware multer untuk mengunggah file
         upload(req, res, async (err) => {
             if (err) {
                 console.error(err);
-                return res.status(500).json({ message: "Error uploading file"});
+                return res.status(500).json({ message: "Error uploading file" });
             }
 
-            await review.update({
-                // Melakukan update atribut yang diizinkan 
-                message_review: message_review,
-                number_review: number_review,
-                photo_review: req.file ? req.files.map(file => file.buffer) : review.photo_review,
-            });
+            // Check if a file is uploaded and update the photo_review
+            if (req.file) {
+                review.photo_review = req.file.buffer;
+            }
 
-            res.status(200).json({
-                message: "Update Review Successfully",
-                data: review,
-            });
+            try {
+                // Save the updated review
+                await review.save();
+
+                res.status(200).json({
+                    message: "Update Review Successfully",
+                    data: review,
+                });
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ message: "Internal Server Error" });
+            }
         });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal Server Error"});
+        res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
+
+
 
 const deleteReview = async (req, res) => {
     try {
