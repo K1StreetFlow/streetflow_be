90c31554bf2533095d11d5be5bf1d98516af3e22
Saeful Mu'minin <saeful2026027@gmail.com>
2023-11-16 14:30:14 +0700

    menghapus

--- src/routes/authAdminRoutes.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { login, logout } = require("../controllers/authAdminController");
-const { verifyTokenCookieAdmin } = require("../middleware/verifyToken");
-const router = express.Router();
-
-router.post("/login", login);
-router.post("/logout", verifyTokenCookieAdmin, logout);
-
-module.exports = router;
