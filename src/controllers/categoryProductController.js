d9b655f0562898d512bbdcab9b04613334be98ef
ReCodink <muhammadraihan1607@gmail.com>
2023-11-20 10:47:52 +0700

    Update Category Products

--- src/controllers/categoryProductController.js
+++ src/controllers/categoryProductController.js
@@ -38,10 +38,10 @@ const getAllCategories = async (req, res) => {
 };
 
 const getCategoryById = async (req, res) => {
-  const { categoryId } = req.params;
+  const { id } = req.params;
 
   try {
-    const category = await CategoryProduct.findByPk(categoryId);
+    const category = await CategoryProduct.findByPk(id);
 
     if (!category) {
       return res.status(404).json({ error: "Category not found" });
