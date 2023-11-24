65b04b523a5ae176e67820bbda32edac23112c60
ReCodink <muhammadraihan1607@gmail.com>
2023-11-24 23:29:13 +0700

    Update Shipping Routes

--- src/routes/shipping.routes.js
+++ src/routes/shipping.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { getAllShipping, getShippingById, createShipping, updateShipping, deleteShipping, updateShippingAndOrderList } = require("../controllers/shipping.controller");
-const { isAdmin } = require("../middleware/verifyToken");
+const {
+	getAllShipping,
+	getShippingById,
+	createShipping,
+	updateShipping,
+	deleteShipping,
+	updateShippingAndOrderList,
+} = require("../controllers/shipping.controller");
+// const { isAdmin } = require("../middleware/verifyToken");
 const router = express.Router();
 
-router.get("/", isAdmin, getAllShipping);
+router.get("/", getAllShipping);
 router.get("/:id", getShippingById);
 router.post("/create", createShipping);
 router.put("/update/:id", updateShipping);
