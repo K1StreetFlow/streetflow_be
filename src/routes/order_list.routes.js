d63b5189114f585444a8d82b0c45495ff261c2b6
ReCodink <muhammadraihan1607@gmail.com>
2023-11-25 00:01:47 +0700

    Update Order List

--- src/routes/order_list.routes.js
+++ src/routes/order_list.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { getAllOrder, getOrderById, updateOrder, createOrder, deleteOrder } = require("../controllers/order_list.controller");
+const { getAllOrder, getOrderById, updateOrder, createOrderAndShipping, deleteOrder } = require("../controllers/order_list.controller");
 const { isAdminOrSelf } = require("../middleware/adminMiddleware");
 const { isAdmin } = require("../middleware/verifyToken");
 const router = express.Router();
 router.get("/", getAllOrder);
 router.get("/:id", getOrderById);
 router.put("/update/:id", updateOrder);
-router.post("/create", createOrder);
+router.post("/create", createOrderAndShipping);
 router.delete("/delete/:id", deleteOrder);
 
 module.exports = router;
