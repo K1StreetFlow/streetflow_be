b1477c4572e1e0d608d4ae0f53b19970cb020e15
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2023-11-25 15:19:50 +0700

    menambahkan auth di payment, cart, cart-detail dan category

--- src/routes/categoryProduct.routes.js
+++ src/routes/categoryProduct.routes.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { getAllCategories, getCategoryById, createCategory, updateCategory, getAllCategoriesWithPagination, deleteCategory } = require("../controllers/categoryProductController");
+const {
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  getAllCategoriesWithPagination,
+  deleteCategory,
+} = require("../controllers/categoryProductController");
 
 const { isAdminOrSelf } = require("../middleware/adminMiddleware.js");
+const { isAdmin } = require("../middleware/verifyToken.js");
 
 // CRUD routes
 
 router.get("/", getAllCategories);
 router.get("/pagination", getAllCategoriesWithPagination);
 router.get("/:id", getCategoryById);
-router.post("/", isAdminOrSelf, createCategory);
-router.put("/:id", isAdminOrSelf, updateCategory);
-router.delete("/:id", isAdminOrSelf, deleteCategory);
+router.post("/", isAdmin, createCategory);
+router.put("/:id", isAdmin, updateCategory);
+router.delete("/:id", isAdmin, deleteCategory);
 
 module.exports = router;
