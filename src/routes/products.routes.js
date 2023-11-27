3ec1477465f213b2e10b619a31f82b75a2a93d0e
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2023-11-28 00:40:55 +0700

    menambahkan seeder cart, cart detail dan relasi product ke review product

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts, getProductById, getAllProductsWithPagination, createProduct, updateProduct, deleteProduct } = require("../controllers/products.controller");
+const {
+  getAllProducts,
+  getProductById,
+  getAllProductsWithPagination,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/products.controller");
 const { isAdmin } = require("../middleware/verifyToken.js");
 
 // CRUD routes
-router.get("/", isAdmin, getAllProducts);
+router.get("/", getAllProducts);
 // AKSES PAGINATION SAYA BIKIN 10 DATA YANG TAMPIL SETIAP PAGE
 router.get("/pagination", getAllProductsWithPagination);
 router.get("/:id", getProductById);
