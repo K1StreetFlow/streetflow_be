af10f666f52300c012c8492d1ab7c5a6f3e8ee3d
Saeful Mu'minin <saeful2026027@gmail.com>
2023-11-28 01:22:23 +0700

    ++asset

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,7 +4,7 @@ const { getAllProducts, getProductById, getAllProductsWithPagination, createProd
 const { isAdmin } = require("../middleware/verifyToken.js");
 
 // CRUD routes
-router.get("/", isAdmin, getAllProducts);
+router.get("/", getAllProducts);
 // AKSES PAGINATION SAYA BIKIN 10 DATA YANG TAMPIL SETIAP PAGE
 router.get("/pagination", getAllProductsWithPagination);
 router.get("/:id", getProductById);
